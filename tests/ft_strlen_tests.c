2018c0963ca4925250337b2686efa7472e132cab
kfujita <kfujita@student.42tokyo.jp>
2026-03-25 18:54:09 +0900

    add empty str test to strlen

--- tests/ft_strlen_tests.c
+++ tests/ft_strlen_tests.c
@@ -5,6 +5,8 @@ extern size_t ft_strlen(const char *s);
 int	main(void) {
 	size_t len;
 
+	len = ft_strlen("");
+	printf("len: %ld\n", len);
 	len = ft_strlen("Hello, World!");
 	printf("len: %ld\n", len);
 	return 0;
